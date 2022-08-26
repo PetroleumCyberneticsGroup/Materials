0f068371f0718ba3b92a0c429464036af1e544b4
I Gusti Agung Gede Angga <agung_gede@outlook.com>
2022-08-26 10:40:28 +0200

    Create RunnerC-GA.py

---
+++ CollaborativeAlgorithms/RunnerC-GA.py
@@ -0,0 +1,21 @@
+import os
+
+
+coop = [True, False]
+coop_str = ["clone"]
+nr = 1000
+list_args = []
+
+for i in range(len(coop)):
+    if coop[i]:
+        for j in range(len(coop_str)):
+            for k in range(nr):
+                args = " " + str(coop[i]) + " " + str(coop_str[j]) + " " + str(k + 1)
+                list_args.append(args)
+    else:
+        for k in range(nr):
+            args = " " + str(coop[i]) + " " + "-" + " " + str(k + 1)
+            list_args.append(args)
+
+for i in range(len(list_args)):
+    os.system("python3.8 C-GA.py" + list_args[i])
